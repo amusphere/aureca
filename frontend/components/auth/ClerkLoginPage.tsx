d99fa4c6edb28e4cbd34d2f9498b814b783024f0
shuto-S <shuto@amusphere.dev>
2025-07-18 21:53:04 +0900

    update login page

--- frontend/components/auth/ClerkLoginPage.tsx
+++ frontend/components/auth/ClerkLoginPage.tsx
@@ -1,32 +1,13 @@
 import AuthedLayout from "@/components/components/commons/AuthedLayout";
-import { Button } from "@/components/components/ui/button";
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/components/ui/card";
 import HomePage from "@/components/pages/HomePage";
-import { SignedIn, SignedOut, SignInButton } from "@clerk/nextjs";
+import { SignedIn, SignedOut } from "@clerk/nextjs";
+import ClerkLoginForm from "./ClerkLoginForm";
 
 export default function ClerkLoginPage() {
   return (
     <>
       <SignedOut>
-        <div className="flex flex-col items-center justify-center min-h-screen">
-          <Card className="w-1/4 text-center">
-            <CardHeader>
-              <CardTitle className="text-2xl my-2">
-                {process.env.APP_NAME}
-              </CardTitle>
-              <CardDescription>
-                A template for building Next.js applications.
-              </CardDescription>
-            </CardHeader>
-            <CardContent>
-              <SignInButton mode="modal" signUpFallbackRedirectUrl="/api/users/create">
-                <Button className="w-full">
-                  Log In
-                </Button>
-              </SignInButton>
-            </CardContent>
-          </Card>
-        </div>
+        <ClerkLoginForm />
       </SignedOut>
       <SignedIn>
         <AuthedLayout>
