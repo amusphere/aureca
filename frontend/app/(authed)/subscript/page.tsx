6ed63c816204d6603b9b12fb45fba80c0b0f5206
shuto-S <shuto@amusphere.dev>
2025-08-10 23:01:22 +0900

    feat: Update SubscriptionPage title to Japanese and move style definition

--- frontend/app/(authed)/subscript/page.tsx
+++ frontend/app/(authed)/subscript/page.tsx
@@ -1,10 +1,18 @@
+"use client"
+
 import { PricingTable } from "@clerk/nextjs";
 
 export default function SubscriptionPage() {
   return (
     <div className="px-2">
+      <style jsx global>{`
+        .cl-pricingTableCardDescription {
+          white-space: pre-line;
+        }
+      `}</style>
+
       <div className="pt-6 mb-4">
-        <h1 className="text-3xl font-bold">Subscription Plans</h1>
+        <h1 className="text-3xl font-bold">プラン内容</h1>
       </div>
       <PricingTable />
     </div>
